9af9c8f25c607e74b80ec6d6256657268d50a11a
Gustavo <gustavo.bt@me.com>
2016-08-12 11:59:44 -0500

    Centralize authorization

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DevicesController < ApiController
   skip_before_action :authorize, only: :create
 
   def create
-    if resource.save
+    if @device.save
       render :show, status: 201
     else
       head :not_found
@@ -10,7 +10,7 @@ class Api::V1::DevicesController < ApiController
   end
 
   def destroy
-    if resource.destroy
+    if @device.destroy
       head :ok
     else
       render json: { errors: @device.errors.full_messages }
@@ -19,11 +19,11 @@ class Api::V1::DevicesController < ApiController
 
   private
 
-  def device_params
-    params.require(:device).permit(:email, :password)
+  def new_resource_params
+    device_params
   end
 
-  def resource
-    @device ||= params[:id].present? ? Device.find(params[:id]) : Device.new(device_params)
+  def device_params
+    params.require(:device).permit(:email, :password)
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,8 @@
 class ApiController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  attr_accessor :resource
-  before_action :authorize
+  before_action :resource,
+                :authorize
 
   def current_user
     current_device.user
@@ -10,17 +10,46 @@ class ApiController < ActionController::API
 
   private
 
-  def current_device
-    @current_device ||= authenticate_with_http_token do |token, _|
-                          Device.active.find_by(token: token) || Device.new
-                        end
-  end
+    def current_device
+      @current_device ||= authenticate_with_http_token do |token, _|
+                            Device.active.find_by(token: token) || Device.new
+                          end
+    end
 
-  def authorize
-    head :unauthorized unless authorization.authorize?(controller_name, controller_action, resource)
-  end
+    def authorize
+      head :unauthorized unless authorization.allow?(controller_name, action_name, resource)
+    end
 
-  def authorization
-    @authorization ||= Authorization.new(current_user)
-  end
+    def authorization
+      @authorization ||= Authorization.new(current_user)
+    end
+
+    def resource
+      instance_variable_get("@#{ ivar_name }") ||
+      instance_variable_set("@#{ ivar_name }", (find_resource || build_resource))
+    end
+
+    def ivar_name
+      controller_name.singularize
+    end
+
+    def param_key
+      resource_class.model_name.param_key
+    end
+
+    def find_resource
+      resource_class.find(params[:id]) if params[:id].present?
+    end
+
+    def resource_class
+      controller_name.classify.constantize
+    end
+
+    def build_resource
+      resource_class.new(new_resource_params)
+    end
+
+    def new_resource_params
+      {}
+    end
 end
