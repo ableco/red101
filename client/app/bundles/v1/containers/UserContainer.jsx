cc75d20fa1fb360c5d1afe92d65361b127052a57
DanteCavero <dante.cavero@gmail.com>
2016-08-12 12:10:14 -0500

    first view of profile. wait for a pr with the new structure

---
+++ client/app/bundles/v1/containers/UserContainer.jsx
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import UserViewComponent from '../components/UserViewComponent';
+
+// Simple example of a React "smart" component
+export default class UserContainer extends React.Component {
+  static propTypes = {
+    user: PropTypes.any
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    // How to set initial state in ES6 class syntax
+    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+    // this.state = { name: this.props.name };
+  }
+
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <UserViewComponent user={this.props.user} />
+      </div>
+    );
+  }
+}
