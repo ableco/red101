f0561e0e2651d303f8095926732916092938fe8b
DanteCavero <dante.cavero@gmail.com>
2016-08-12 11:26:44 -0500

    add perfil route with helloword example

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :devices, only: :create
     end
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "perfil", to: "user#index"
+
 end
