2e48c404f429f680ed77a606ef131c564fb3a6ed
Gustavo <gustavo.bt@me.com>
2016-08-17 15:10:07 -0500

    Store device token on a cookie

--- client/app/bundles/v1/lib/headers.js
+++ client/app/bundles/v1/lib/headers.js
@@ -1,7 +1,8 @@
 import xr from 'xr';
+import Cookies from 'js-cookie';
 
-export default function requestHeaders(getState) {
-  const { token } = getState().railsContext;
+export default function headers() {
+  const token = Cookies.get('token');
   return Object.assign({}, xr.defaults.headers, {
     'Authorization': `Token token=${token}`
   });
