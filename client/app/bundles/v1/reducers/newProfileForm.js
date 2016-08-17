57c0ff08175f0f8e05b8769d79dbdb82b33ee294
Gustavo <gustavo.bt@me.com>
2016-08-17 17:02:36 -0500

    Initialize with empty string instead of null

--- client/app/bundles/v1/reducers/newProfileForm.js
+++ client/app/bundles/v1/reducers/newProfileForm.js
@@ -2,10 +2,10 @@ import * as actions from '../actions';
 
 export const initialState = {
   formData: {
-    first_name: null,
-    last_name:  null,
-    email:      null,
-    password:   null
+    first_name: '',
+    last_name:  '',
+    email:      '',
+    password:   ''
   },
   errors: {},
   submitting: false
@@ -15,17 +15,17 @@ export default function newProfileForm(state = initialState, action = null) {
   const { type, profile, error } = action;
 
   switch (type) {
-    case actions.UPDATE_PROFILE_FORM: {
+    case actions.default.UPDATE_PROFILE_FORM: {
       return Object.assign({}, state, {
         formData: Object.assign({}, state.formData, { [action.key]: action.value }),
         errors:   Object.assign({}, state.errors,   { [action.key]: null }),
       });
     }
-    case actions.CREATE_PROFILE_START:
+    case actions.default.CREATE_PROFILE_START:
       return Object.assign({}, state, { submitting: true });
-    case actions.CREATE_PROFILE_SUCCESS:
+    case actions.default.CREATE_PROFILE_SUCCESS:
       return Object.assign({}, state, { submitting: false });
-    case actions.CREATE_PROFILE_ERROR:
+    case actions.default.CREATE_PROFILE_ERROR:
       return Object.assign({}, state, {
         errors:     Object.assign({}, state.errors, action.errors),
         submitting: false,
