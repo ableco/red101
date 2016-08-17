db24c892eee41b4073fa4d774b2d925f1cb5ca93
Gustavo <gustavo.bt@me.com>
2016-08-17 18:01:15 -0500

    Export headers as object

--- client/app/bundles/v1/lib/headers.js
+++ client/app/bundles/v1/lib/headers.js
@@ -1,9 +1,9 @@
 import xr from 'xr';
 import Cookies from 'js-cookie';
 
-export default function headers() {
-  const token = Cookies.get('token');
-  return Object.assign({}, xr.defaults.headers, {
-    'Authorization': `Token token=${token}`
-  });
-}
+const token   = Cookies.get('token');
+const headers = Object.assign({}, xr.defaults.headers, {
+  'Authorization': `Token token=${token}`
+});
+
+export default headers;
