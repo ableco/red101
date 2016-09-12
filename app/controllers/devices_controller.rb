48a7e75b36c7cc50d9d980d9c8b373ba58c2999d
Gustavo <gustavo.bt@me.com>
2016-09-12 07:35:27 -0500

    Implement base login

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,9 +1,27 @@
 class DevicesController < ApplicationController
   skip_before_action :authorize, only: %i(new create)
 
+  include Rest
+
   def new
+    @device = Device.new
   end
 
-  def create
+  private
+
+  def find_resource
+    current_device
+  end
+
+  def permitted_attributes
+    %i(email password)
+  end
+
+  def after_path
+    current_user ? profile_path : login_path
+  end
+
+  def created
+    cookies.signed[:token] = @device.token
   end
 end
