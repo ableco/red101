64c5d45651b4e9dc168515f73b8494145e70a58c
Gustavo <gustavo.bt@me.com>
2016-09-15 07:22:45 -0500

    Add diagnostics routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   root 'root#index'
 
-  get :search, to: 'root#search'
-  get :login,  to: 'devices#new'
-  get :logout, to: 'devices#destroy'
-
-  get '/go/:slug', to: 'root#go', as: :go
+  get :search,    to: 'root#search'
+  get :admin,     to: 'root#admin'
+  get :register,  to: 'root#register'
+  get :login,     to: 'devices#new'
+  get :logout,    to: 'devices#destroy'
+  get 'go/:slug', to: 'root#go', as: :go
 
   resource  :profile
   resource  :device
+  resources :diagnostics
   resources :topics
   resources :templates
   resources :questions
-  resources :users
   resources :materials
+  resources :users, only: %i(index show)
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
