900e1c0445a2ac5cf740434fc251249888cdc72d
Gustavo <gustavo.bt@me.com>
2016-09-15 20:34:42 -0500

    Finish and score diagnostics

--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -9,12 +9,34 @@ class Diagnostic < ApplicationRecord
   validates :template, presence: true
 
   after_create :create_answers
+  before_save  :finish_and_score, if: :answered?
+
+  scope :pending, -> { where(finished_at: nil) }
+
+  accepts_nested_attributes_for :answers
+
+  def finished?
+    persisted? && finished_at.present?
+  end
+
+  def pending?
+    !finished?
+  end
+
+  def answered?
+    answers.all?(&:option_id)
+  end
 
   private
 
-    def create_answers
-      template.choose_random_question_ids.each do |question_id|
-        answers.create(question_id: question_id)
-      end
+  def create_answers
+    template.choose_random_question_ids.each do |question_id|
+      answers.create(question_id: question_id)
     end
+  end
+
+  def finish_and_score
+    self.finished_at = Time.current
+    self.score       = answers.correct.count
+  end
 end
